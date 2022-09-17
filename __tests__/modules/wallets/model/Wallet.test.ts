8a87b15e5985f62660c7fa4f656b7828eeef8d62
Halex Viotto Gomes <halexx002@gmail.com>
2022-09-17 18:25:51 -0400

    test: it should return a default wallet

---
+++ __tests__/modules/wallets/model/Wallet.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from '@jest/globals';
+import Wallet from '../../../../src/modules/wallets/model/Wallet';
+
+describe('Model - Wallet', () => {
+  it('should return a default wallet', () => {
+    const wallet = new Wallet();
+    const defaultDate = new Date('2000-01-01');
+
+    const expectedDefaultWallet: Wallet = {
+      id: 'no id',
+      email: 0,
+      sms: 0,
+      whatsapp: 0,
+      createdAt: defaultDate,
+      updatedAt: defaultDate,
+    };
+
+    expect(wallet).toEqual(expectedDefaultWallet);
+  });
+});
