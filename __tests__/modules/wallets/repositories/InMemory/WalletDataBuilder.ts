b97d1abf943d6af59bec15b5f65bf56a7fc01cde
Halex Viotto Gomes <halexx002@gmail.com>
2022-09-17 21:43:59 -0400

    chore: create wallet data builder

---
+++ __tests__/modules/wallets/repositories/InMemory/WalletDataBuilder.ts
@@ -0,0 +1,37 @@
+interface WalletData {
+  id: string;
+  userId: string;
+  email: Number;
+  sms: Number;
+  whatsapp: Number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export default class WalletDataBuilder {
+  private readonly walletData;
+
+  private constructor() {
+    const dateNow = new Date('2022-01-01');
+
+    this.walletData = {
+      id: '4321',
+      userId: '1234',
+      email: 0,
+      sms: 0,
+      whatsapp: 0,
+      createdAt: dateNow,
+      updatedAt: dateNow,
+    };
+  }
+
+  static aWalletData(): WalletDataBuilder {
+    return new WalletDataBuilder();
+  }
+
+  build(): WalletData {
+    return this.walletData;
+  }
+}
+
+export { WalletData };
