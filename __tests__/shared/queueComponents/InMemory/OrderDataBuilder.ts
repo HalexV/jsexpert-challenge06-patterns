e768fc192c8315a69ad94909b197b7f0b85e057f
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-03 16:04:14 -0400

    chore(test): create order data builder and mother object

---
+++ __tests__/shared/queueComponents/InMemory/OrderDataBuilder.ts
@@ -0,0 +1,27 @@
+import { Order } from '../../../../src/shared/queueComponents/InMemory/InMemoryOrderQueue';
+
+export default class OrderDataBuilder {
+  private readonly orderData: Order;
+
+  private constructor() {
+    this.orderData = {
+      type: 'cc',
+      userId: '1234',
+      transactionCode: '123asd',
+      credits: [
+        {
+          id: '0',
+          quantity: 5,
+        },
+      ],
+    };
+  }
+
+  static anOrderData(): OrderDataBuilder {
+    return new OrderDataBuilder();
+  }
+
+  build(): Order {
+    return this.orderData;
+  }
+}
---
+++ __tests__/shared/queueComponents/InMemory/OrderDataMother.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-extraneous-class */
+
+import { Order } from '../../../../src/shared/queueComponents/InMemory/InMemoryOrderQueue';
+import OrderDataBuilder from './OrderDataBuilder';
+
+export default class OrderDataMother {
+  static valid(): Order {
+    return OrderDataBuilder.anOrderData().build();
+  }
+}
