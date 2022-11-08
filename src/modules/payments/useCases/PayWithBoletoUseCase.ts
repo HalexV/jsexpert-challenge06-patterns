baf297db4824575e06807f227b2a35cb6975abbb
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-08 16:10:29 -0400

    feat: it should call orderQueue add with correct arguments

--- src/modules/payments/useCases/PayWithBoletoUseCase.ts
+++ src/modules/payments/useCases/PayWithBoletoUseCase.ts
@@ -1,6 +1,10 @@
 import IPaymentComponent, {
   Product,
 } from '../../../shared/paymentComponents/IPaymentComponent';
+import {
+  Credit,
+  Order,
+} from '../../../shared/queueComponents/InMemory/InMemoryOrderQueue';
 import IQueueComponent from '../../../shared/queueComponents/IQueueComponent';
 
 interface IRequest {
@@ -12,13 +16,27 @@ interface IRequest {
 export default class PayWithBoletoUseCase {
   constructor(
     private readonly paymentComponent: IPaymentComponent,
-    orderQueue: IQueueComponent
+    private readonly orderQueue: IQueueComponent
   ) {}
 
   async execute({ userId, customerName, productList }: IRequest): Promise<any> {
-    await this.paymentComponent.payWithBoleto({
+    const boletoResponse = await this.paymentComponent.payWithBoleto({
       customerName,
       productList,
     });
+
+    const credits: Credit[] = productList.map((product) => ({
+      id: product.id,
+      quantity: product.quantity,
+    }));
+
+    const order: Order = {
+      type: 'bol',
+      credits,
+      transactionCode: boletoResponse.boletoCode,
+      userId,
+    };
+
+    await this.orderQueue.add(order);
   }
 }
